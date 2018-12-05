ff62fafdd9d6983023ec4b4af07c5bade382a94f
king <2607640909@qq.com>
2018-12-05 11:37:58 +0800

    add 1.java

---
+++ 1.java
@@ -0,0 +1,14 @@
+package com.offcn.util;
+
+import org.springframework.web.client.RestTemplate;
+
+public class TestMobile {
+
+	public static void main(String[] args) {
+		
+		RestTemplate template = new RestTemplate();
+		Message forObject = template.getForObject("http://localhost:8080/WebServiceCallerloc/service/seach/get/15843118494",Message.class);
+		System.out.println(forObject);
+	}
+
+}
